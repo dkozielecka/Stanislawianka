062bcc18839b7419c15c5cff20573dedb35e8462
dkozielecka <pf3gj4dh5>
2021-05-10 12:41:51 +0200

    add active menu element style

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-const buttons = document.querySelectorAll(".btn");
+const buttons = Array.prototype.slice.call(document.querySelectorAll(".btn"));
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     buttons
       .filter((btn) => btn !== button)
-      .forEach((btn) => btn.classList.remove(".active"));
+      .forEach((btn) => btn.classList.remove("active"));
 
-    button.classList.add(".active");
+    button.classList.add("active");
   });
 });
-console.log(buttons);
